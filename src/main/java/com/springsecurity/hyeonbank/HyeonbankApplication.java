dd13b7f506640bf2bc38c3e8877a4d39d0036c93
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-01 20:48:56 +0900

    Initial commit

---
+++ src/main/java/com/springsecurity/hyeonbank/HyeonbankApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.hyeonbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeonbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyeonbankApplication.class, args);
+	}
+
+}
