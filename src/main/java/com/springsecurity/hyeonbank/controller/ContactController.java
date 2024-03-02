b3c1f8911a8543ba82f218a7fd568496674749f6
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-02 19:22:24 +0900

    securtiy를 이용해 웹을 만들 떄 필요한 controller 생성

---
+++ src/main/java/com/springsecurity/hyeonbank/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.springsecurity.hyeonbank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String getContactInquiryDetails() {
+        return "Inquiry details are saved to the DB";
+    }
+}
