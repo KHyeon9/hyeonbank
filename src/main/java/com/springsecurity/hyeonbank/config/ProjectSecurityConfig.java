64100cec5a0ed54ddf2298561ac23eb68b642da7
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-07 19:59:33 +0900

    db테스트전 scurity 테스트하기 위해서 InMemoryUserDetailsManager를 이용한 부분 주석처리 및  passwordEncoder 작성

--- src/main/java/com/springsecurity/hyeonbank/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/hyeonbank/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +17,53 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests(request -> request
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards")
                         .authenticated() // 위 주소들을 로그인 해야 볼 수 있도록 함
-                        .requestMatchers("/notices", "/contact")
+                        .requestMatchers("/notices", "/contact", "/register")
                         .permitAll() // 위 두 주소만 로그인 없이 볼 수 있도록 함
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("read")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+//
+//    UserDetails admin = User.withUsername("admin")
+//            .password("12345")
+//            .authorities("admin")
+//            .build();
+//
+//    UserDetails user = User.withUsername("user")
+//            .password("12345")
+//            .authorities("read")
+//            .build();
+//
+//    return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
