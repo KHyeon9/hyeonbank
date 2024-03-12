5a6642d81f46312fe458fd357c59902fd57386af
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-12 20:10:25 +0900

    cors관련 설정을 구현

--- src/main/java/com/springsecurity/hyeonbank/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/hyeonbank/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.springsecurity.hyeonbank.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.file.ConfigurationSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,19 +10,38 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
+        http.cors(
+                cors -> cors.configurationSource(
+                                new CorsConfigurationSource() {
+                                    @Override
+                                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                         CorsConfiguration config = new CorsConfiguration();
+                                         config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                                         config.setAllowedMethods(Collections.singletonList("*"));
+                                         config.setAllowCredentials(true);
+                                         config.setAllowedHeaders(Collections.singletonList("*"));
+                                         config.setMaxAge(3600L);
+                                         return config;
+                                    }
+                                }
+                        )
+                ).csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards", "/user")
                         .authenticated() // 위 주소들을 로그인 해야 볼 수 있도록 함
                         .requestMatchers("/notices", "/contact", "/register")
                         .permitAll() // 위 두 주소만 로그인 없이 볼 수 있도록 함
                 )
-                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
         return http.build();
@@ -30,4 +51,5 @@ public class ProjectSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
