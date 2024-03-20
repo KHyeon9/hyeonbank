8e878ad0b4aa1d4d4bd05e7181867bdf392d19f0
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-21 00:01:49 +0900

    jwt를 프론트에서 받을 수 있도록 필터를 생성

---
+++ src/main/java/com/springsecurity/hyeonbank/filter/JWTTokenGeneratorFilter.java
@@ -0,0 +1,63 @@
+package com.springsecurity.hyeonbank.filter;
+
+import com.springsecurity.hyeonbank.constants.SecurityConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            // SecurityConstants에서 jwt key를 가져와 인코딩 방식을 변경하기 위해 byte형식으로 바꿔서 utf-8로 변경 
+            SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+            String jwt = Jwts.builder().issuer("Hyeon Bank").subject("JWT Token")
+                    .claim("username", authentication.getName())
+                    .claim("authorities", populateAuthorities(authentication.getAuthorities()))
+                    // 생성 시간 설정
+                    .issuedAt(new Date())
+                    // 만료 시간 설정
+                    .expiration(new Date((new Date()).getTime() + 30000000))
+                    // 서명에 사용될 key를 추가
+                    .signWith(key).compact();
+            // 요청의 헤더에 jwt토큰 포함
+            response.setHeader(SecurityConstants.JWT_HEADER, jwt);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // jwt token이 /user주소에서만 생성되도록 함
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !request.getServletPath().equals("/user");
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
+        // 권한을 스트링으로 변환해서 반환
+        Set<String> authoritiesSet = new HashSet<>();
+
+        for (GrantedAuthority authority : collection) {
+            authoritiesSet.add(authority.getAuthority());
+        }
+
+        return String.join(",", authoritiesSet);
+    }
+}
