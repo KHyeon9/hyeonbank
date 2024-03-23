8ec6320945e73a419afdceeed51a6b7c2d99ba76
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-24 00:16:32 +0900

    PreFilter와 PostFilter를 사용하기 위해 입출력 값을 컬렉션 인터페이스를 사용하는 타입으로 변경

--- src/main/java/com/springsecurity/hyeonbank/controller/ContactController.java
+++ src/main/java/com/springsecurity/hyeonbank/controller/ContactController.java
@@ -2,9 +2,13 @@ package com.springsecurity.hyeonbank.controller;
 
 import com.springsecurity.hyeonbank.model.Contact;
 import com.springsecurity.hyeonbank.repository.ContactRepository;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -17,10 +21,18 @@ public class ContactController {
     }
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    // filter 보안을 하려면 입력 값과 리턴 값이 컬랙션 인터페이스여야 함
+    //@PreFilter("filterObject.contactName != 'Test'")
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        List<Contact> returnContacts = new ArrayList<>();
+        returnContacts.add(contact);
+
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
