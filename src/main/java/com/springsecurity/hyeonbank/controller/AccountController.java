28f594e9bf691b7fa2fcaf2f77d1bd444fd90282
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-03-30 19:47:19 +0900

    인증 서버를 통해서 인증하고 값을 찾도록 메소드 변경 및 로그인 컨트롤러 메소드 삭제

--- src/main/java/com/springsecurity/hyeonbank/controller/AccountController.java
+++ src/main/java/com/springsecurity/hyeonbank/controller/AccountController.java
@@ -1,23 +1,38 @@
 package com.springsecurity.hyeonbank.controller;
 
 import com.springsecurity.hyeonbank.model.Accounts;
+import com.springsecurity.hyeonbank.model.Customer;
 import com.springsecurity.hyeonbank.repository.AccountsRepository;
+import com.springsecurity.hyeonbank.repository.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
 
     private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
 
-    public AccountController(AccountsRepository accountsRepository) {
+    public AccountController(AccountsRepository accountsRepository, CustomerRepository customerRepository) {
         this.accountsRepository = accountsRepository;
+        this.customerRepository = customerRepository;
     }
 
     @GetMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestParam int id) {
-        Accounts accounts = accountsRepository.findByCustomerId(id);
-        return accounts;
+    public Accounts getAccountDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+
+        if (customers != null && !customers.isEmpty()) {
+            Accounts accounts = accountsRepository.findByCustomerId(customers.get(0).getId());
+
+            if (accounts != null) {
+                return accounts;
+            }
+        }
+
+        return null;
     }
 }
